fff9bbce9916d113ed3de7054412c060f19b9b07
Tim Severien <timseverien@gmail.com>
2017-08-23 20:28:36 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const kontraster = require('kontraster');
+
+// 1. Open site with puppeteer
+
+// 2. Inject code to remove all text
+// 2.1 Take screenshot (base)
+// 2.2 Remove injected code
+
+// 3. Inject code to remove headings
+// 3.1 Take screenshot (headings)
+// 3.2 Remove injected code
+
+// 4. Inject code to remove plain text
+// 4.1 Take screenshot (text)
+// 4.2 Remove injected code
+
+// 5. Put base + headings in kontraster
+// 6. Put base + text in kontraster
+// 7. Optically merge output from step 5 and 6
